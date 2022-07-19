b744c81b48d41bd5aa304daef9d04d46e849cf0f
Tsvetan Tsvetanov <tsvetanov.19@gmail.com>
2022-07-19 09:59:18 +0300

    create unit tests

--- tests/Controller/ProgressControllerTest.php
+++ tests/Controller/ProgressControllerTest.php
@@ -1,1 +1,56 @@
 <?php
+
+namespace App\Tests\CoreLogic;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ProgressControllerTest extends WebTestCase
+{
+    public function testStatusMethodReturns200()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/statuses/1/2/2022-07-15T15:52:01+00:00/2022-07-16T15:52:01+00:00');
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function testWrongDurationType()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/statuses/notnumber/2/3/4');
+        $this->assertEquals(400, $client->getResponse()->getStatusCode());
+        $this->assertStringContainsString("Duration must be an integer!", $client->getResponse()->getContent());
+    }
+
+    public function testNegativeDurationValue()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/statuses/-100/2/3/4');
+        $this->assertEquals(400, $client->getResponse()->getStatusCode());
+        $this->assertStringContainsString("Duration must be positive!", $client->getResponse()->getContent());
+    }
+
+    public function testProgressOutOfScope()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/statuses/1/200/3/4');
+        $this->assertEquals(400, $client->getResponse()->getStatusCode());
+        $this->assertStringContainsString("Progress in % must be an integer between 0 and 100!", $client->getResponse()->getContent());
+    }
+
+    public function testInvalidStartDate()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/statuses/1/2/2022-07-11 15:52:01+00:00/2022-07-15T15:52:01+00:00');
+        $this->assertEquals(400, $client->getResponse()->getStatusCode());
+        $this->assertStringContainsString("Date of creation in wrong format, must use RFC3339!", $client->getResponse()->getContent());
+    }
+
+    public function testInvalidSDueDate()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/statuses/1/2/2022-07-17T15:52:01+00:00/17-07-2022');
+        $this->assertEquals(400, $client->getResponse()->getStatusCode());
+        $this->assertStringContainsString("Due date in wrong format, must use RFC3339!", $client->getResponse()->getContent());
+    }
+
+}
--- tests/CoreLogic/CalculatorTest.php
+++ tests/CoreLogic/CalculatorTest.php
@@ -2,7 +2,64 @@
 
 namespace App\Tests\CoreLogic;
 
-class CalculatorTest extends \PHPUnit\Framework\TestCase
+use \PHPUnit\Framework\TestCase;
+use App\CoreLogic\Calculator;
+
+/*
+    На 05.05.2022 преподавател поставя задача на студент, да завърши определен видеокурс до 10.05.2022 най-късно.
+    На 11.06.2022 е планиран изпит по дисциплината. Общото видеосъдържание е с обем 12 часа. От това задание могат да
+    се пресметнат среднодневната продължителност на видео обучението, което студентът трябва да гледа, за да успее да
+    мине през цялото видеосъдържание и да е подготвен за изпита.
+    Във всеки един момент, студентът може да провери дали се движи с добра скорост, като подава текущият му прогрес по
+    видео съдържанието, т.е. видео материала, който вече е изгледал като процент от всичкият видео материал.
+    Комбинирайки данните, може да се каже бинарно дали прогреса до тук е достатъчен, за да завърши курса до крайната
+    дата или трябва да се увеличи времето за гледане на ден, за да се постигне целта.
+ */
+
+
+class CalculatorTest extends TestCase
 {
+
+    public function testProgressIsComplete()
+    {
+        $duration = 123456;
+        $progress = 100;
+        $startDate = new \DateTimeImmutable('2022-07-15T15:52:01+00:00');
+        $dueDate = new \DateTimeImmutable('2022-07-19T15:52:01+00:00');
+
+        $calculator = new Calculator($duration, $progress, $startDate, $dueDate);
+        $result = $calculator->calculateOutput();
+        $this->assertEquals(0, $result['needed_daily_learning_time']);
+    }
+
+
+    public function testCourseInProgressComplete()
+    {
+        $duration = 123456;
+        $progress = 14;
+        $startDate = new \DateTimeImmutable('2022-07-15T15:52:01+00:00');
+        $dueDate = new \DateTimeImmutable('2022-07-19T15:52:01+00:00');
+
+        $calculator = new Calculator($duration, $progress, $startDate, $dueDate);
+        $result = $calculator->calculateOutput();
+        $this->assertEquals(2, $result['needed_daily_learning_time']);
+        dd([$calculator, $result]);
+        dd($result);
+    }
+
+//    public function testDueDateIsBeforeStartDate()
+//    {
+//
+//    }
+//
+//    public function testDueDateIsBeforeNow()
+//    {
+//
+//    }
+//
+//    public function testNowIsBeforeStartDate()
+//    {
+//
+//    }
 
 }
